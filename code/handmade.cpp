baff50390d229fa378ac8530ca00fe2ff3fd62b4
MeMetoCoco3 <torresmartinezvidal@gmail.com>
2025-03-04 00:19:24 +0100

    002

--- code/handmade.cpp
+++ code/handmade.cpp
@@ -6,10 +6,97 @@
 =========================================================================
  */
 
+/*typedef struct tagWNDCLASSA {
+  UINT      style;
+  WNDPROC   lpfnWndProc;
+  int       cbClsExtra;
+  int       cbWndExtra;
+  HINSTANCE hInstance;
+  HICON     hIcon;
+  HCURSOR   hCursor;
+  HBRUSH    hbrBackground;
+  LPCSTR    lpszMenuName;
+  LPCSTR    lpszClassName;
+} WNDCLASSA, *PWNDCLASSA, *NPWNDCLASSA, *LPWNDCLASSA;*/
+
 #include <windows.h>
 
-int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-                     LPSTR lpCmdLine, int nShowCmd) {
-  MessageBox(0, "This is the shit!", "Handmade", MB_OK | MB_ICONINFORMATION);
+LRESULT CALLBACK MainWindowCallback(HWND Window, UINT Message, WPARAM WParam,
+                                    LPARAM LParam) {
+  LRESULT Result = 0;
+  switch (Message) {
+
+  case WM_SIZE: {
+    OutputDebugString("WM_SIZE\n");
+  } break;
+  case WM_DESTROY: {
+    OutputDebugString("WM_DESTROY\n");
+  } break;
+  case WM_CLOSE: {
+    OutputDebugString("WM_CLOSE\n");
+  } break;
+  case WM_ACTIVATEAPP: {
+    OutputDebugString("WM_ACTIVATEAPP\n");
+  } break;
+  case WM_PAINT: {
+    PAINTSTRUCT Paint;
+    HDC DeviceContext = BeginPaint(Window, &Paint);
+    int X = Paint.rcPaint.left;
+    int Y = Paint.rcPaint.top;
+    int Width = Paint.rcPaint.right - Paint.rcPaint.left;
+    int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+    static DWORD Operation = WHITENESS;
+
+    PatBlt(DeviceContext, X, Y, Width, Height, Operation);
+    if (Operation == WHITENESS) {
+      Operation = BLACKNESS;
+    } else {
+      Operation = WHITENESS;
+    }
+    EndPaint(Window, &Paint);
+
+  } break;
+  default: {
+    // OutputDebugString("DEFAULT\n");
+    Result = DefWindowProc(Window, Message, WParam, LParam);
+  } break;
+  }
+  return (Result);
+}
+int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PrevInstance,
+                     LPSTR CommandLine, int ShowCode) {
+  WNDCLASS WindowClass = {}; // Sets all to 0
+
+  WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+  WindowClass.lpfnWndProc = MainWindowCallback;
+  WindowClass.hInstance = Instance;
+  // WindowClass.hIcon = ;
+  WindowClass.lpszClassName = "HandmadeHeroWindowClass";
+  if (RegisterClass(&WindowClass)) {
+    HWND WindowHandle = CreateWindowEx(
+        0, WindowClass.lpszClassName, "Handmade Hero",
+        WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT,
+        CW_USEDEFAULT, CW_USEDEFAULT, 0, 0, Instance, 0);
+
+    if (WindowHandle) {
+      MSG Message;
+      for (;;) {
+
+        BOOL MessageResult = GetMessageA(&Message, 0, 0, 0);
+        if (MessageResult > 0) {
+          TranslateMessage(&Message);
+          DispatchMessage(&Message);
+        } else {
+          break;
+        }
+      }
+
+    } else {
+      // TODO: Logging
+    }
+  } else {
+    // TODO: Logging
+  }
+
   return 0;
 }
